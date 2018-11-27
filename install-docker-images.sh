97d282dbc92e70071c7d1dc5467dc6510f14b8e9
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-11-27 08:20:52 +0800

    [add] install registry

--- install-docker-images.sh
+++ install-docker-images.sh
@@ -2,6 +2,7 @@
 HERE=`cd $(dirname $0); pwd`
 
 sudo docker pull gitlab/gitlab-ce
+sudo docker pull registry
 
 # the initial root password is 5iveL!fe, after login you are required to set a
 # unique password.
