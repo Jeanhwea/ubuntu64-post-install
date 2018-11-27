aec20ee50a44674defc479075f4da2abd0a3ffdf
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-11-27 11:13:32 +0800

    [change] debain url to ubuntu

--- install-docker.sh
+++ install-docker.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software
 sudo apt-get remove docker docker-engine docker.io
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian \
+   "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
