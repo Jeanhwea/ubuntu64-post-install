ebf071e67e81d37f975bd760d990c598118ccf20
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-11-28 18:57:33 +0800

    [add] some breaking lines

--- install-docker.sh
+++ install-docker.sh
@@ -4,13 +4,18 @@ HERE=`cd $(dirname $0); pwd`
 # see https://mirrors.tuna.tsinghua.edu.cn/help/docker-ce/
 
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
 sudo apt-get remove docker docker-engine docker.io
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+
 sudo apt-get update
+
 sudo apt-get install -y docker-ce
 
 # accelerate downloading
