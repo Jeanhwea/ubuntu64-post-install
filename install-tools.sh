40a5e78172eb74d70c7a987616b5cd5fefc2c73e
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-12-23 16:19:08 +0800

    add dependency for bazel

--- install-tools.sh
+++ install-tools.sh
@@ -6,6 +6,9 @@ sudo apt-get install -y openssh-server tmux htop ncdu build-essential ack
 
 sudo apt-get install -y cmake git unzip zip
 
+# for bazel
+sudo apt-get install pkg-config zip g++ zlib1g-dev unzip
+
 sudo apt-get install -y python-dev python3-dev python-pip python3-pip
 
 sudo apt-get install linux-headers-$(uname -r)
