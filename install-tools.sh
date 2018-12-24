ff736bdaaf1293031b7e803a9aa8a5c0ff5a3aae
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-12-24 18:24:07 +0800

    add bazel deps

--- install-tools.sh
+++ install-tools.sh
@@ -6,8 +6,12 @@ sudo apt-get install -y openssh-server tmux htop ncdu build-essential ack
 
 sudo apt-get install -y cmake git unzip zip
 
+# for opencv
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
 # for bazel
-sudo apt-get install pkg-config zip g++ zlib1g-dev unzip
+sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip
 
 sudo apt-get install -y python-dev python3-dev python-pip python3-pip
 
