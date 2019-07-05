6aa94a09331fad65e699ea8bf1a8a3c7d1f3e556
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-07-05 10:05:06 +0800

    add install basic

--- install-apt.sh
+++ install-apt.sh
@@ -2,13 +2,6 @@
 HERE=`cd $(dirname $0); pwd`
 
 # see https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
-
-# setup apt
 [ ! -f /etc/apt/sources.list.old ] && sudo mv /etc/apt/sources.list /etc/apt/sources.list.old
 sudo cp $HERE/sources.list /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get upgrade -y
-
-# git config file
-cp $HERE/.gitconfig ~/.gitconfig
-cp $HERE/.tmux.conf ~/.tmux.conf
+sudo apt-get update && upgrade -y
---
+++ install-basic.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+HERE=`cd $(dirname $0); pwd`
+
+# copy files
+cp $HERE/.gitconfig ~/.gitconfig
+cp $HERE/.tmux.conf ~/.tmux.conf
+
+# basic tool
+sudo apt-get install -y \
+     ack \
+     apt-transport-https \
+     build-essential \
+     ca-certificates \
+     curl \
+     git \
+     gnupg2 \
+     htop \
+     ncdu \
+     net-tools \
+     openssh-server \
+     software-properties-common \
+     tmux \
+     unzip \
+     vim \
+     zip
+
+# setup docker
+sudo apt-get remove docker docker-engine docker.io
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get install -y docker-ce
+sudo mkdir -p /etc/docker && cp $HERE/daemon.json /etc/docker/daemon.json
+sudo service docker restart
